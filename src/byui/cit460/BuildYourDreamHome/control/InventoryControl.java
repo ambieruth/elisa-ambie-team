25be8f6e08cb5fbf09b2df58f95af5fa8d7163ca
ElisaHutchings <ElisaHutchings@Hutchings>
2015-05-28 21:15:25 -0700

    5/28/2015 - Created InventoryControl layer and tested

---
+++ src/byui/cit460/BuildYourDreamHome/control/InventoryControl.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit460.BuildYourDreamHome.control;
+
+/**
+ *
+ * @author ElisaHutchings
+ */
+public class InventoryControl {
+
+    public double calcLumberSize(double height, double width, double length) {
+
+    if (height < 0) {
+        return -1;
+    }
+    
+    if (width <0) {
+        return -1;
+    }
+    
+    if (length < 0) {
+        return -1;
+    }
+    
+    double dimension = length * width * height;
+    
+    return dimension;
+            
+}
+}
